3e586bde6d1984d7fba01098ab764cbfae650f8f
Luke Rieff <l.rieff@ommnia-systems.nl>
2026-04-16 12:24:14 +0200

    A.1. afgerond

--- Coureurs die in alle races van het 2024 seizoen zijn gefinished.sql
+++ Coureurs die in alle races van het 2024 seizoen zijn gefinished.sql
@@ -1,2 +1,73 @@
-SELECT *
-FROM Circuit;
+------------------------------------------------------------------------------------------------------------------------
+-- Option 1
+------------------------------------------------------------------------------------------------------------------------
+
+/*
+NOTICE:
+Due to the absence of a column that specifies the total number of laps for each race, the assumption has been made
+that at least one driver completed the entire race, hence the highest observed Lap can be used to infer the actual
+number of laps; for both queries these results have been validated with actually known race-data, and this assumption
+is correct for the year 2024.
+*/
+
+WITH
+    -- The total amount of (inferred) laps for each race in the season of 2024.
+    RaceLaps AS (SELECT Race.RaceId      AS RaceId
+                      , MAX(Result.Laps) AS Laps
+                 FROM Result
+                          INNER JOIN Race ON Race.RaceId = Result.RaceId
+                 WHERE Race.RaceYear = 2024
+                 GROUP BY Race.RaceId),
+    -- The amount of laps each driver has completed in each 2024 race.
+    DriverLaps AS (SELECT Result.DriverId AS DriverId
+                        , Result.RaceId   AS RaceId
+                        , Result.Laps     AS Laps
+                   FROM Driver
+                            INNER JOIN Result ON Result.DriverId = Driver.DriverId
+                            INNER JOIN Race ON Race.RaceId = Result.RaceId
+                   WHERE Race.RaceYear = 2024),
+    -- The amount by which each driver has completed each race (in the range of 0 to 1).
+    RaceCompletion AS (SELECT DriverLaps.DriverId                                             AS DriverId
+                            , DriverLaps.RaceId                                               AS RaceId
+                            , (CAST(DriverLaps.Laps AS FLOAT) / CAST(RaceLaps.Laps AS FLOAT)) AS Completion
+                       FROM DriverLaps
+                                INNER JOIN RaceLaps ON RaceLaps.RaceId = DriverLaps.RaceId),
+    -- The lowest observed completion in all races for each driver.
+    MinCompletion AS (SELECT RaceCompletion.DriverId        AS DriverId
+                           , MIN(RaceCompletion.Completion) AS MinCompletion
+                      FROM RaceCompletion
+                      GROUP BY RaceCompletion.DriverId)
+-- Select the drivers with (at least) 90% completion for all races, and show their names.
+SELECT CONCAT_WS(' ', Driver.Firstname, Driver.Lastname)                         AS Name
+     , CONCAT(CAST(ROUND(MinCompletion.MinCompletion, 2) * 100 AS VARCHAR), '%') AS Completion
+FROM MinCompletion
+         INNER JOIN Driver ON Driver.DriverId = MinCompletion.DriverId
+WHERE MinCompletion.MinCompletion >= 0.9
+ORDER BY MinCompletion.MinCompletion DESC
+
+------------------------------------------------------------------------------------------------------------------------
+-- Option 2
+------------------------------------------------------------------------------------------------------------------------
+
+WITH
+    -- Determine the completion of each driver per race.
+    RaceCompletion
+        AS (SELECT CAST(Result.Laps AS FLOAT) / MAX(Result.Laps) OVER (PARTITION BY Race.RaceId) AS Completion
+                 , Race.RaceId                                                                   AS RaceId
+                 , Driver.DriverId                                                               AS DriverId
+            FROM Driver
+                     INNER JOIN Result ON Result.DriverId = Driver.DriverId
+                     INNER JOIN Race ON Race.RaceId = Result.RaceId
+            WHERE Race.RaceYear = 2024),
+    -- Determine the completion per driver (in the minimum).
+    DriverCompletion AS (SELECT MIN(RaceCompletion.Completion) AS MinCompletion
+                              , RaceCompletion.DriverId        AS DriverId
+                         FROM RaceCompletion
+                         GROUP BY RaceCompletion.DriverId)
+-- Select only the drivers that have at least 90% completion.
+SELECT CONCAT_WS(' ', Driver.Firstname, Driver.Lastname)                            AS Name
+     , CONCAT(CAST(ROUND(DriverCompletion.MinCompletion, 2) * 100 AS VARCHAR), '%') AS Completion
+FROM DriverCompletion
+         INNER JOIN Driver ON Driver.DriverId = DriverCompletion.DriverId
+WHERE DriverCompletion.MinCompletion >= 0.9
+ORDER BY DriverCompletion.MinCompletion DESC
