67e96bb492a30a40a36cbf513ce61bc22387ae1a
Luke Rieff <l.rieff@ommnia-systems.nl>
2026-04-20 12:50:14 +0200

    Add the alternative for the 6th assignment.

--- A/6.sql
+++ A/6.sql
@@ -1,9 +1,35 @@
 -- Option 1
+WITH DriverWins AS (SELECT Result.DriverId AS DriverId
+                         , Race.RaceYear   AS RaceYear
+                         , COUNT(1)        AS Wins
+                    FROM Result
+                             INNER JOIN Race ON Race.RaceId = Result.RaceId
+                    WHERE Result.Position = 1
+                    GROUP BY Result.DriverId, Race.RaceYear),
+     DriverRaces AS (SELECT Result.DriverId AS DriverId
+                          , Race.RaceYear   AS RaceYear
+                          , COUNT(1)        AS Races
+                     FROM Result
+                              INNER JOIN Race ON Race.RaceId = Result.RaceId
+                     GROUP BY Result.DriverId, Race.RaceYear)
+SELECT CONCAT_WS(' ', Driver.Firstname, Driver.Lastname) AS Driver
+     , DriverRaces.RaceYear                              AS Season
+     , DriverRaces.Races                                 AS Races
+     , COALESCE(DriverWins.Wins, 0)                      AS Wins
+     , CONCAT(CAST(ROUND(100.0 * COALESCE(DriverWins.Wins, 0) / NULLIF(DriverRaces.Races, 0), 2) AS DECIMAL(10, 2)),
+              '%')                                       AS Percentage
+FROM DriverRaces
+         INNER JOIN Driver ON Driver.DriverId = DriverRaces.DriverId
+         LEFT JOIN DriverWins
+                   ON DriverWins.DriverId = DriverRaces.DriverId
+                       AND DriverWins.RaceYear = DriverRaces.RaceYear
+ORDER BY COALESCE(DriverWins.Wins, 0) * 1.0 / DriverRaces.Races DESC;
+
+-- Option 2
 SELECT CONCAT_WS(' ', Driver.Firstname, Driver.Lastname) AS Driver
      , Race.RaceYear                                     AS Season
      , COUNT(1)                                          AS Races
      , SUM(IIF(Result.Position = 1, 1, 0))               AS Wins
-     -- Force as DECIMAL to make the rounding work properly, for some reason it normally doesn't.
      , CONCAT(CAST(ROUND(100.0 * SUM(IIF(Result.Position = 1, 1, 0)) / NULLIF(COUNT(*), 0), 2) AS DECIMAL(10, 2)),
               '%')                                       AS Percentage
 FROM Result
