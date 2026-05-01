9bbe897c3ac437ac2e8a685966a76e6a59e41ff6
Luke Rieff <l.rieff@ommnia-systems.nl>
2026-05-01 14:06:46 +0200

    add render script'

---
+++ render.sh
@@ -0,0 +1,9 @@
+pandoc README.md \
+  -o README.pdf \
+  --toc \
+  --number-sections \
+  -V title="Uitwerking Casus RDI" \
+  -V author="Luke A.C.A. Rieff" \
+  -V date="01-05-2026" \
+  -V geometry:margin=2.5cm \
+  -V pagestyle=plain
